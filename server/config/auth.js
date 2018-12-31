24fd3ed06fad80cfa06efceff5d4122c305dcc93
mariapesm <youandmecanyousee@hotmail.com>
2018-12-31 22:07:46 +0200

    Create auth.js

---
+++ server/config/auth.js
@@ -0,0 +1,13 @@
+'use strict';
+require('dotenv').config();
+module.exports = {
+  twitterAuth: {
+    consumerKey: process.env.CONSUMERKEY,
+    consumerSecret: process.env.CONSUMERSECRET,
+    callbackURL: process.env.CALLBACKURL
+  },
+  databaseUrl: process.env.DATABASEURL,
+  secret: process.env.SECRET,
+  sessionSecret: process.env.SESSSECRET,
+  yelpToken: process.env.YELPTOKEN
+};
